dba645bc114125b4c359ec46904b1ab5aeb36f86
Agustin Fraile <agustinfraile98@gmail.com>
2022-09-15 04:46:26 -0300

    agregando estilos a la app

--- 07-React-Estilos/homework/src/components/Card.jsx
+++ 07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,34 @@
-import React from 'react';
+import styles from './card.module.css'
 
-export default function Card(props) {
+
+export default function Card({max, min, name, img, onClose}) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+    <div className={styles.cardContainer}>
+
+      <button
+        className={styles.cardContainer_button}
+        onClick={onClose}
+      >
+        X
+      </button>
+
+      <div>
+        <h4>{name}</h4>
+      </div>
+      
+      <div>
+        <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt={`imagen de ${img}`}  />
+      </div>
+
+      <div>
+        <p>Maxima: {max}</p>
+      </div>
+      
+      <div>
+        <p>Minima: {min}</p>
+      </div>
+      
+    </div>
+  )
 };
--- 07-React-Estilos/homework/src/components/Cards.jsx
+++ 07-React-Estilos/homework/src/components/Cards.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import Card from './Card';
+import styles from'./cards.module.css'
 
-export default function Cards(props) {
+
+export default function Cards({cities}) {
   // acá va tu código
   // tip, podés usar un map
-  return <div>Cards Component</div>
+
+  return (
+      <div className={styles.cardsContainer}>
+        {
+          cities.map(card => 
+            <Card 
+              name={card.name}
+              img={card.weather[0].icon}
+              max={card.main.temp_max}
+              min={card.main.temp_min}
+              onClose={() => alert(card.name)}
+              key={card.id}
+            />
+          )
+        }     
+      </div>
+    )
 };
