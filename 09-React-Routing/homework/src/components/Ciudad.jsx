026bad727b607b6bdae4c8e1c855688a4f147a11
Agustin Fraile <agustinfraile98@gmail.com>
2022-09-21 14:13:21 -0300

    agregando react-router-dom al proyecto

---
+++ 09-React-Routing/homework/src/components/Ciudad.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './ciudad.module.css'
+
+
+export default function Ciudad() {
+  return (
+    <div>Ciudad</div>
+  )
+}
--- 09-React-Routing/homework/src/components/NavBar.jsx
+++ 09-React-Routing/homework/src/components/NavBar.jsx
@@ -1,33 +1,33 @@
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import SearchBar from "./SearchBar"
 
 export default function NavBar({onSearch}) {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+
+            <Navbar.Brand>Clima-App</Navbar.Brand>
+
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                    Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                </NavDropdown.Item>
-                </NavDropdown>
-            </Nav>
+              <Nav className="me-auto">
+                
+                <Link to='/'>
+                  <Nav.Item>Home</Nav.Item>
+                </Link>
+                
+                <Link to='/about'>
+                  <Nav.Item>About</Nav.Item>
+                </Link>
+
+              </Nav>
+              
             </Navbar.Collapse>
         </Container>
+        
         <SearchBar
           onSearch={onSearch}
         />
