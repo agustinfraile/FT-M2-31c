e0709a33a078134fe08f2e14beeb84edcff0ea09
Agustin Fraile <agustinfraile98@gmail.com>
2022-09-22 12:24:41 -0300

    agregando estado al formulario

--- 10-React-Forms/homework/src/Form.jsx
+++ 10-React-Forms/homework/src/Form.jsx
@@ -1,9 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+export function validate(input) {
+  let errors = {};
+  if (!input.username) {
+    errors.username = 'Username is required';
+  } else if (!/\S+@\S+\.\S+/.test(input.username)) {
+    errors.username = 'Username is invalid';
+  }
+  
+  if(!input.password) {
+    errors.password = 'Password is required';
+  } else if((!/(?=.*[0-9])/.test(input.password))) {
+    errors.password = 'Password is invalid';
+  }
+
+  return errors;
+};
+
 
 export default function  Form() {
+
+  const [input, setInput] = React.useState({
+    username: '',
+    password: '',
+  })
+
+  const [errors, setErrors] = React.useState({})
+
+  const handleInputChange = function(e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.taret.value
+    });
+
+    setErrors(validate({
+      ...input,
+      [e.target.name]: e.target.value
+    }));
+  }
+
+  // const onSubmit=(e) => {
+  //   e.preventDefault();
+  // }
+
+
   return (
+    <form>
       <div>
-        Componente Form
+        <label>Username:</label>
+        <input 
+          className={errors.username && 'danger'}
+          type="text" name="username" 
+          value={input.username} 
+          onChange={ handleInputChange }
+        />
+        {errors.username && (
+          <p className="danger">{errors.username}</p>
+        )}
       </div>
+      <div>
+        <label>Password</label>
+        <input 
+          className={errors.password && 'danger'}
+          type="password" name="password" 
+          value={input.password} 
+          onChange={ handleInputChange }
+        />
+        {errors.password && (
+          <p className="danger">{errors.password}</p>
+        )}
+      </div>
+
+      <input type="submit" value='Agregar' />
+    </form>
   )
 }
