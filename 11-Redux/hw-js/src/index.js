e7929266ce729ebbc2df6eba286738922c970f73
Agustin Fraile <agustinfraile98@gmail.com>
2022-09-26 16:59:23 -0300

    modificando ejercicio

--- 11-Redux/hw-js/src/index.js
+++ 11-Redux/hw-js/src/index.js
@@ -1,30 +1,39 @@
 const { createStore } = require('redux');
 const contador = require('./reducer');
-const { incremento, decremento } = require('./actions');
+const { incremento, decremento, incrementoImpar, incrementoAsync } = require('./actions');
 
 // En esta linea creamos nuestro store. Pasandole como parametro nuestro Reducer
-var store;
+var store = createStore(contador);
 
 // Obtenemos el elemento con el id `valor`.
-var valor;
+var valor = document.getElementById("valor");
 
 // Esta funcion nos va a servir para actualizar nuestro DOM con el valor que tengamos en nuestro Store.
 // En el primer render y cada vez que nos subscribamos al Store.
 // Utilizamos el elemento obtenido arriba para mostrar el State.
 function renderContador() {
   // Obtenemos la propiedad 'contador' de nuestro store:
-  
+  var nuevoValor = store.getState().contador;
   // Seteamos el numero obtenido como texto dentro del elemento con id 'valor':
-
+  valor.innerText = nuevoValor;
 }
 
 // Ejecutamos la funcion 'renderContador':
-
+renderContador()
 
 
 // Nos subscribimos al store pasandole la misma funcion. Asi cada vez que llegue una accion, ejecutamos la funcion:
-
+store.subscribe(renderContador)
 
 
 // Por ultimo, utilizamos los botones de nuestro HTML para que cada vez que hagamos click,
 // hagan un dispatch al store de la accion correspondiente:
+const botonIncremento = document.getElementById("incremento");
+const botonDecremento = document.getElementById("decremento");
+const botonImpar = document.getElementById("incrementoImpar");
+const botonAsync = document.getElementById("incrementoAsync");
+
+botonIncremento.onclick = () => store.dispatch(incremento());
+botonDecremento.onclick = () => store.dispatch(decremento());
+botonImpar.onclick = () => store.dispatch(incrementoImpar());
+botonAsync.onclick = () => store.dispatch(setTimeout(incrementoAsync(),3000));
